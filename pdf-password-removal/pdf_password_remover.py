32cb3ea98e849979f31d9e440c384e819c52510b
Siege Songsong <cjgsongsong@gmail.com>
2025-09-13 20:00:14 +0800

    Refactor PDF password remover

--- pdf-password-removal/pdf_password_remover.py
+++ pdf-password-removal/pdf_password_remover.py
@@ -1,7 +1,17 @@
-import pikepdf
+"""Module for removing password protection from secured PDF files."""
+
 import sys
+import pikepdf
 
-file = sys.argv[1]
+def remove_password(filepath: str, password: str) -> None:
+    """Overwrite the password-protected file in specified path as its unprotected version."""
 
-pikepdf.open(file, password=sys.argv[2], allow_overwriting_input=True) \
-       .save(file)
+    pikepdf \
+        .open(
+            filepath,
+            allow_overwriting_input=True,
+            password=password,
+        ) \
+        .save(filepath)
+
+remove_password(sys.argv[1], sys.argv[2])
