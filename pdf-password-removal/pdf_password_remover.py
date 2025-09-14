a81dddd5bc88ba37d1d7e1acdcbfcb8c04262c82
Siege Songsong <cjgsongsong@gmail.com>
2025-09-14 18:33:12 +0800

    Handle unprotected files in PDF password remover

--- pdf-password-removal/pdf_password_remover.py
+++ pdf-password-removal/pdf_password_remover.py
@@ -5,31 +5,39 @@ import sys
 
 import pikepdf
 
-PDF_SEARCH_PATTERN = "/**/*.pdf"
+PDF_SEARCH_PATTERN = '/**/*.pdf'
 
 def remove_password_protection(filepath: str, passwords: list[str]) -> None:
-    """Overwrite the password-protected file in given file path as its unprotected version."""
+    """Overwrite a file as its unprotected version if it is password-protected."""
 
-    did_overwrite = False
+    try:
+        pikepdf.open(filepath)
 
-    for password in passwords:
-        try:
-            pikepdf \
-                .open(
-                    filepath,
-                    allow_overwriting_input=True,
-                    password=password,
-                ) \
-                .save(filepath)
+        print(f'No password protection exists for `{filepath}`.')
+    except pikepdf.PasswordError as has_password_exception:
+        did_overwrite = False
 
-            did_overwrite = True
+        for password in passwords:
+            try:
+                pikepdf \
+                    .open(
+                        filepath,
+                        allow_overwriting_input=True,
+                        password=password,
+                    ) \
+                    .save(filepath)
 
-            break
-        except pikepdf.PasswordError:
-            continue
+                did_overwrite = True
 
-    if not did_overwrite:
-        raise ValueError(f'Given password(s) cannot open `{filepath}`.')
+                break
+            except pikepdf.PasswordError:
+                continue
+
+        if not did_overwrite:
+            raise ValueError(f'Given password(s) cannot open `{filepath}`.') \
+                from has_password_exception
+
+        print(f'Password protection is now removed for `{filepath}`.')
 
 def batch_remove_password_protection(directory: str, passwords: list[str]) -> None:
     """Remove password protection from all PDF files in the specified directory."""
