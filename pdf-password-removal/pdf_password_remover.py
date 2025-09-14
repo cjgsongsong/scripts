0affed04afacedd732dbe325af802360dec9a138
Siege Songsong <cjgsongsong@gmail.com>
2025-09-14 17:54:44 +0800

    Try multiple passwords in PDF password remover

--- pdf-password-removal/pdf_password_remover.py
+++ pdf-password-removal/pdf_password_remover.py
@@ -7,21 +7,34 @@ import pikepdf
 
 PDF_SEARCH_PATTERN = "/**/*.pdf"
 
-def remove_password_protection(filepath: str, password: str) -> None:
+def remove_password_protection(filepath: str, passwords: list[str]) -> None:
     """Overwrite the password-protected file in given file path as its unprotected version."""
 
-    pikepdf \
-        .open(
-            filepath,
-            allow_overwriting_input=True,
-            password=password,
-        ) \
-        .save(filepath)
+    did_overwrite = False
 
-def batch_remove_password_protection(directory: str, password: str) -> None:
+    for password in passwords:
+        try:
+            pikepdf \
+                .open(
+                    filepath,
+                    allow_overwriting_input=True,
+                    password=password,
+                ) \
+                .save(filepath)
+
+            did_overwrite = True
+
+            break
+        except pikepdf.PasswordError:
+            continue
+
+    if not did_overwrite:
+        raise ValueError(f'Given password(s) cannot open `{filepath}`.')
+
+def batch_remove_password_protection(directory: str, passwords: list[str]) -> None:
     """Remove password protection from all PDF files in the specified directory."""
 
     for filepath in glob.iglob(directory + PDF_SEARCH_PATTERN, recursive=True):
-        remove_password_protection(filepath, password)
+        remove_password_protection(filepath, passwords)
 
-batch_remove_password_protection(sys.argv[1], sys.argv[2])
+batch_remove_password_protection(sys.argv[1], [sys.argv[2]])
