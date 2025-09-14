f34b5ed64d4252f0b9ca960799c881cd27216397
Siege Songsong <cjgsongsong@gmail.com>
2025-09-14 22:02:02 +0800

    Refactor functions in PDF password remover

--- pdf-password-removal/pdf_password_remover.py
+++ pdf-password-removal/pdf_password_remover.py
@@ -8,31 +8,7 @@ import pikepdf
 
 PDF_SEARCH_PATTERN = '/**/*.pdf'
 
-def _get_pdf_file_paths(file_path_input: str) -> list[str]:
-    """Get PDF file paths from input string.
-    
-    Return a PDF file path pattern if input is a valid directory.
-    Return the file path input if it is a valid PDF file.
-    Return an empty string otherwise.
-    """
-
-    if file_path_input.endswith('.pdf') and os.path.isfile(file_path_input):
-        return [file_path_input]
-
-    file_paths = []
-
-    if os.path.isdir(file_path_input):
-        file_paths = glob.glob(
-            file_path_input + PDF_SEARCH_PATTERN,
-            recursive=True,
-        )
-
-        if file_paths:
-            return file_paths
-
-    raise ValueError('Given file path has no valid PDF files.')
-
-def remove_password_protection(file_path: str, passwords: list[str]) -> None:
+def _remove_password_protection_per_file(file_path: str, passwords: list[str]) -> None:
     """Overwrite a PDF file as its unprotected version if it is password-protected."""
 
     try:
@@ -64,10 +40,34 @@ def remove_password_protection(file_path: str, passwords: list[str]) -> None:
 
         print(f'Password protection is now removed for `{file_path}`.')
 
-def batch_remove_password_protection(file_path_input: str, passwords: list[str]) -> None:
+def _get_pdf_file_paths(file_path_input: str) -> list[str]:
+    """Get PDF file paths from input string.
+    
+    Return a PDF file path pattern if input is a valid directory.
+    Return the file path input if it is a valid PDF file.
+    Return an empty string otherwise.
+    """
+
+    if file_path_input.endswith('.pdf') and os.path.isfile(file_path_input):
+        return [file_path_input]
+
+    file_paths = []
+
+    if os.path.isdir(file_path_input):
+        file_paths = glob.glob(
+            file_path_input + PDF_SEARCH_PATTERN,
+            recursive=True,
+        )
+
+        if file_paths:
+            return file_paths
+
+    raise ValueError('Given file path has no valid PDF files.')
+
+def remove_password_protection(file_path_input: str, passwords: list[str]) -> None:
     """Remove password protection from all PDF files in the specified directory."""
 
     for file_path in _get_pdf_file_paths(file_path_input):
-        remove_password_protection(file_path, passwords)
+        _remove_password_protection_per_file(file_path, passwords)
 
-batch_remove_password_protection(sys.argv[1], [sys.argv[2]])
+remove_password_protection(sys.argv[1], [sys.argv[2]])
