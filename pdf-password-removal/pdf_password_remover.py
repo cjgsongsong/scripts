e108166cb5cd13f64199b6f98cecaac6b6124fe3
Siege Songsong <cjgsongsong@gmail.com>
2025-09-14 21:39:47 +0800

    Get PDF file paths in PDF password remover

--- pdf-password-removal/pdf_password_remover.py
+++ pdf-password-removal/pdf_password_remover.py
@@ -1,19 +1,44 @@
 """Module for removing password protection from secured PDF files."""
 
 import glob
+import os
 import sys
 
 import pikepdf
 
 PDF_SEARCH_PATTERN = '/**/*.pdf'
 
-def remove_password_protection(filepath: str, passwords: list[str]) -> None:
-    """Overwrite a file as its unprotected version if it is password-protected."""
+def _get_pdf_file_paths(file_path_input: str) -> list[str]:
+    """Get PDF file paths from input string.
+    
+    Return a PDF file path pattern if input is a valid directory.
+    Return the file path input if it is a valid PDF file.
+    Return an empty string otherwise.
+    """
+
+    if file_path_input.endswith('.pdf') and os.path.isfile(file_path_input):
+        return [file_path_input]
+
+    file_paths = []
+
+    if os.path.isdir(file_path_input):
+        file_paths = glob.glob(
+            file_path_input + PDF_SEARCH_PATTERN,
+            recursive=True,
+        )
+
+        if file_paths:
+            return file_paths
+
+    raise ValueError('Given file path has no valid PDF files.')
+
+def remove_password_protection(file_path: str, passwords: list[str]) -> None:
+    """Overwrite a PDF file as its unprotected version if it is password-protected."""
 
     try:
-        pikepdf.open(filepath)
+        pikepdf.open(file_path)
 
-        print(f'No password protection exists for `{filepath}`.')
+        print(f'No password protection exists for `{file_path}`.')
     except pikepdf.PasswordError as has_password_exception:
         did_overwrite = False
 
@@ -21,11 +46,11 @@ def remove_password_protection(filepath: str, passwords: list[str]) -> None:
             try:
                 pikepdf \
                     .open(
-                        filepath,
+                        file_path,
                         allow_overwriting_input=True,
                         password=password,
                     ) \
-                    .save(filepath)
+                    .save(file_path)
 
                 did_overwrite = True
 
@@ -34,15 +59,15 @@ def remove_password_protection(filepath: str, passwords: list[str]) -> None:
                 continue
 
         if not did_overwrite:
-            raise ValueError(f'Given password(s) cannot open `{filepath}`.') \
+            raise ValueError(f'Given password(s) cannot open `{file_path}`.') \
                 from has_password_exception
 
-        print(f'Password protection is now removed for `{filepath}`.')
+        print(f'Password protection is now removed for `{file_path}`.')
 
-def batch_remove_password_protection(directory: str, passwords: list[str]) -> None:
+def batch_remove_password_protection(file_path_input: str, passwords: list[str]) -> None:
     """Remove password protection from all PDF files in the specified directory."""
 
-    for filepath in glob.iglob(directory + PDF_SEARCH_PATTERN, recursive=True):
-        remove_password_protection(filepath, passwords)
+    for file_path in _get_pdf_file_paths(file_path_input):
+        remove_password_protection(file_path, passwords)
 
 batch_remove_password_protection(sys.argv[1], [sys.argv[2]])
