f4b7ad7d2e1d5e2fd7908e6e84222d9ab1bdb737
Siege Songsong <cjgsongsong@gmail.com>
2026-05-27 14:09:47 +0800

    refactor(unlock-pdf): Refactor `MessageEnum`

--- unlock_pdf/src/unlock_pdf/classes.py
+++ unlock_pdf/src/unlock_pdf/classes.py
@@ -1,13 +1,15 @@
 """`unlock-pdf` classes."""
 
 from enum import Enum
+from typing import override
 
 class MessageEnum(Enum):
     """`Enum` wrapper to emulate `StrEnum` behavior for its members with string values."""
+    @override
     def __str__(self) -> str:
         """
-        Override `Enum`'s default `__str__` behavior to return the member's value when accessing
-        said member if said value is a string.
+        Override `Enum`'s default `__str__` behavior to return the member's value
+        when accessing said member if said value is a string.
         """
         return (
             self.value
