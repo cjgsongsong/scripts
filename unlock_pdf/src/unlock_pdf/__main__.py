edc9078de7c18fbbaef26097e8834f5484528e19
Siege Songsong <cjgsongsong@gmail.com>
2026-06-04 23:10:51 +0800

    feat(unlock-pdf): Handle execution as import

--- unlock_pdf/src/unlock_pdf/__main__.py
+++ unlock_pdf/src/unlock_pdf/__main__.py
@@ -1,7 +1,11 @@
 """`unlock-pdf` entry point."""
 
-from unlock_pdf.enumerations import Python
+from unlock_pdf.enumerations import ErrorMessage, Module
 from unlock_pdf.functions import unlock_pdf
 
-if __name__ == Python.DIRECT_EXECUTION_MODULE:
+if __name__ in [
+    module.value for module in Module
+]:
     unlock_pdf()
+else:
+    raise RuntimeError(ErrorMessage.NO_INVALID_EXECUTION)
