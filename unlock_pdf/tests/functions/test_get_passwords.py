69dc9ddbc6992ecb453eedf78b0668e0ac9ebd2e
Siege Songsong <cjgsongsong@gmail.com>
2026-06-06 01:07:04 +0800

    refactor(unlock-pdf): Refactor `_get_pdf_file_paths` tests to dedicated file

--- unlock_pdf/tests/functions/test_get_passwords.py
+++ unlock_pdf/tests/functions/test_get_passwords.py
@@ -7,7 +7,7 @@ from pytest import (
     mark,
     raises
 )
-from typing import Callable
+from tests.utilities import generate_mock_get_unique_inputs
 from unlock_pdf.functions import _get_passwords
 from unlock_pdf.types import Passwords
 
@@ -18,31 +18,6 @@ from unlock_pdf.types import Passwords
 #
 # See https://pytest.org/en/7.4.x/reference/reference.html#pytest.MonkeyPatch.setattr.
 import unlock_pdf.functions as target
-
-def _generate_mock_get_unique_inputs(mock_passwords: Passwords) -> Callable[[str], Passwords]:
-    """
-    Generate `_mock_get_unique_inputs`
-    based on given mock passwords.
-
-    :param mock_passwords: Mock ordered list of unique passwords
-                           to attempt unlocking each PDF file with. 
-    :returns: Mock function of `unlock-pdf.functions._get_unique_inputs`.
-    """
-
-    def _mock_get_unique_inputs(prompt: str) -> Passwords:
-        """
-        Mock function of `unlock-pdf.functions._get_unique_inputs` that
-        returns mock passwords to attempt unlocking each PDF file with.
-        
-        :param prompt: Prompt detailing what inputs are being asked of the user.
-        :returns Mock ordered list of unique passwords to attempt unlocking each PDF file with.
-        """
-
-        assert prompt == "Enter every password to attempt unlocking each PDF file with."
-
-        return mock_passwords
-
-    return _mock_get_unique_inputs
 
 def test_get_passwords_raises_exception(monkeypatch: MonkeyPatch) -> None:
     """
@@ -56,7 +31,10 @@ def test_get_passwords_raises_exception(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(
         name = "_get_unique_inputs",
         target = target,
-        value = _generate_mock_get_unique_inputs([])
+        value = generate_mock_get_unique_inputs(
+            mock_inputs = [],
+            test_prompt = "Enter every password to attempt unlocking each PDF file with."
+        )
     )
 
     with raises(
@@ -89,7 +67,10 @@ def test_get_passwords_returns_passwords(
     monkeypatch.setattr(
         name = "_get_unique_inputs",
         target = target,
-        value = _generate_mock_get_unique_inputs(test_passwords)
+        value = generate_mock_get_unique_inputs(
+            mock_inputs = test_passwords,
+            test_prompt = "Enter every password to attempt unlocking each PDF file with."
+        )
     )
 
     assert _get_passwords() == test_passwords
