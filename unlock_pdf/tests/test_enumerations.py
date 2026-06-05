0e3bfc60f74d8a4cb9a3c68d5d46533db1fa7e89
Siege Songsong <cjgsongsong@gmail.com>
2026-06-05 21:26:54 +0800

    refactor(unlock-pdf): Refactor error message generation tests to dedicated file

--- unlock_pdf/tests/test_enumerations.py
+++ unlock_pdf/tests/test_enumerations.py
@@ -1,40 +1,10 @@
 """Tests for `unlock-pdf` enumerations."""
 
 from pytest import mark, raises
-from unlock_pdf.enumerations import (
-    ErrorMessage,
-    FileState,
-    LogMessage
-)
+from unlock_pdf.enumerations import FileState, LogMessage
 
 class TestMessageGeneration:
     """Tests for message generation."""
-
-    def test_generate_failed_overwrite_error_message_errs_with_invalid_file_path(self) -> None:
-        """
-        Assert that `_generate_failed_overwrite_error_message`
-        raises an appropriate exception
-        when given an invalid file path.
-        """
-
-        with raises(
-            expected_exception = ValueError,
-            match = "File path must be a non-empty string"
-        ):
-            ErrorMessage.FAILED_OVERWRITE("")
-
-    def test_generate_failed_overwrite_error_message_generates_with_valid_file_path(self) -> None:
-        """
-        Assert that `_generate_failed_overwrite_error_message`
-        generates an error message for failed overwrite that
-
-        - has the correct format, and
-        - includes the given file path
-
-        when given a valid file path.
-        """
-
-        assert ErrorMessage.FAILED_OVERWRITE("test.pdf") == "Unlocking test.pdf failed."
 
     def test_generate_file_state_count_log_message_errs_with_invalid_arguments(self) -> None:
         """
