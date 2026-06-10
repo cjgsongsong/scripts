5a18fac0432c82cd689e1548bc4841cc9fdfa62b
Siege Songsong <cjgsongsong@gmail.com>
2026-06-10 22:46:26 +0800

    refactor(unlock-pdf): Refactor string generator enumeration to dedicated file

---
+++ unlock_pdf/src/unlock_pdf/classes/string_generator_enumeration.py
@@ -0,0 +1,18 @@
+"""String generator enumeration class."""
+
+from enum import Enum
+from typing import override
+
+class StrGenEnum(Enum):
+    """`Enum` wrapper to emulate `StrEnum` behavior for its members with string values."""
+    @override
+    def __str__(self) -> str:
+        """
+        Override `Enum`'s default `__str__` behavior to return the member's value
+        when accessing said member if said value is a string.
+        """
+        return (
+            self.value
+            if isinstance(self.value, str)
+            else super().__str__()
+        )
