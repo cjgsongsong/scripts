c7a3e61914007ef64308feb913d7933db7633fd2
Siege Songsong <cjgsongsong@gmail.com>
2026-06-10 23:19:51 +0800

    feat(unlock-pdf): Type-check methods

--- unlock_pdf/src/unlock_pdf/classes/string_generator_enumeration.py
+++ unlock_pdf/src/unlock_pdf/classes/string_generator_enumeration.py
@@ -1,11 +1,14 @@
 """String generator enumeration class."""
 
 from enum import Enum
+from typeguard import typechecked
 from typing import override
 
 class StrGenEnum(Enum):
     """`Enum` wrapper to emulate `StrEnum` behavior for its members with string values."""
+
     @override
+    @typechecked
     def __str__(self) -> str:
         """
         Override `Enum`'s default `__str__` behavior to return the member's value
