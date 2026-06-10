74e72803f810cb9228ce7d7376135a088f56a01c
Siege Songsong <cjgsongsong@gmail.com>
2026-06-10 23:08:05 +0800

    refactor(unlock-pdf): Refactor string generation enumeration test to dedicated file

---
+++ unlock_pdf/tests/classes/test_string_generator_enumeration.py
@@ -0,0 +1,19 @@
+"""Tests for string generator enumeration class."""
+
+from unlock_pdf.enumerations import StrGenEnum
+
+def test_string_generator_enumeration_stringifies() -> None:
+    """
+    Assert that a string generator enumeration
+    stringifies its member
+    based on whether said member's value is a string or not.
+    """
+
+    class _TestStrGenEnum(StrGenEnum):
+        """Mock message enumeration."""
+
+        NON_STRING = 0
+        STRING = "test-value"
+
+    assert str(_TestStrGenEnum.NON_STRING) == "_TestStrGenEnum.NON_STRING"
+    assert str(_TestStrGenEnum.STRING) == "test-value"
