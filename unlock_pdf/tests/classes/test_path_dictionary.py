915adddfc416fd058ce748bbb4c517abfc454c5d
Siege Songsong <cjgsongsong@gmail.com>
2026-06-11 14:08:32 +0800

    test(unlock-pdf): Test path dictionary initialization

--- unlock_pdf/tests/classes/test_path_dictionary.py
+++ unlock_pdf/tests/classes/test_path_dictionary.py
@@ -3,19 +3,44 @@
 from pytest import mark
 from unlock_pdf.classes.path_dictionary import PathDictionary
 from unlock_pdf.enumerations import FileState
+from unlock_pdf.types import InitialPathDictionary, Paths
 
-def test_path_dictionary_initializes() -> None:
+@mark.parametrize(
+    "test_initial_path_dictionary, test_initial_pdf_file_paths",
+    [
+        (
+            {
+                file_state: ["test.pdf"]
+                for file_state in FileState
+            },
+            ["test.pdf"]
+        ),
+        (None, [])
+    ]
+)
+def test_path_dictionary_initializes(
+    test_initial_path_dictionary: InitialPathDictionary,
+    test_initial_pdf_file_paths: Paths
+) -> None:
     """
     Assert that a path dictionary
-    initializes its keys as file states and 
-    its values as empty PDF file paths.
+    initializes
+    
+    - its keys as file states, and 
+    - its values as either
+      - an empty list of PDF file paths, or
+      - an initial list of PDF file paths.
+
+    :param test_initial_path_dictionary: Initial dictionary
+                                         that maps file states with PDF file paths.
+    :param test_initial_pdf_file_paths: Initial list of PDF file paths of every file state.
     """
 
-    test_path_dictionary = PathDictionary()
+    test_path_dictionary = PathDictionary(test_initial_path_dictionary)
 
     for file_state in FileState:
         assert file_state in test_path_dictionary.keys()
-        assert test_path_dictionary[file_state] == []
+        assert test_path_dictionary[file_state] == test_initial_pdf_file_paths
 
 @mark.parametrize("test_file_state", FileState)
 def test_path_dictionary_groups_file_path_by_file_state(test_file_state: FileState) -> None:
