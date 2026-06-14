1a34b52a9c2e264f5b11a32764c06fabaf88b70b
Siege Songsong <cjgsongsong@gmail.com>
2026-06-14 23:41:36 +0800

    refactor(unlock-pdf): Refactor entry point

--- unlock_pdf/src/unlock_pdf/__main__.py
+++ unlock_pdf/src/unlock_pdf/__main__.py
@@ -6,4 +6,6 @@ from unlock_pdf.functions import unlock_pdf
 if __name__ == Module.DIRECT_EXECUTION:
     unlock_pdf()
 else:
-    raise RuntimeError(ErrorMessage.NO_INVALID_EXECUTION)
+    raise RuntimeError(
+        ErrorMessage.NO_INVALID_EXECUTION
+    )
