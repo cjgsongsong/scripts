55b6740558711213daf794ea37d025b0b9c3cfa4
Siege Songsong <cjgsongsong@gmail.com>
2026-06-14 22:43:54 +0800

    test(unlock-pdf) Test stringification of generated string

--- unlock_pdf/tests/classes/test_string_generator_enumeration.py
+++ unlock_pdf/tests/classes/test_string_generator_enumeration.py
@@ -12,8 +12,16 @@ def test_string_generator_enumeration_stringifies() -> None:
     class _TestStrGenEnum(StrGenEnum):
         """Test message enumeration."""
 
+        @staticmethod
+        def _test_generate_string() -> str:
+            """Generate a test generated string."""
+
+            return "test-generated-string"
+
         NON_STRING = 0
-        STRING = "test-value"
+        STRING = "test-string"
+        STRING_GENERATOR = _test_generate_string
 
     assert str(_TestStrGenEnum.NON_STRING) == "_TestStrGenEnum.NON_STRING"
-    assert str(_TestStrGenEnum.STRING) == "test-value"
+    assert str(_TestStrGenEnum.STRING) == "test-string"
+    assert str(_TestStrGenEnum.STRING_GENERATOR()) == "test-generated-string"
